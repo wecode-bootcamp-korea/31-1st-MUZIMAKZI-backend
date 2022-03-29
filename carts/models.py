63f57a3ff00baae0521c7db9dc21e82ae627a014
gleehave <gleehave@gmail.com>
2022-03-29 17:44:23 +0900

    [이강호][add] cart 모델 작성

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Carts(models.Model):
+    user = models.OneToOneField('users.User', on_delete=models.Model)
+    product_option_id = models.ForeignKey('products.products_options', on_delete=models.Model, related_name='carts')
+    quantity = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'carts'
