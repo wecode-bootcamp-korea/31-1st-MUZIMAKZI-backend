65bf18a33dca66180722ca7a11c7fe8c32ca9bb6
gleehave <gleehave@gmail.com>
2022-03-29 16:39:06 +0900

    [이강호][add] users 모델 작성 완료

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+    create_at  = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class User(TimeStampedModel):
+    first_name   = models.CharField(max_length=20)
+    last_name    = models.CharField(max_length=30)
+    email        = models.CharField(max_length=200)
+    password     = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return self.email
+
