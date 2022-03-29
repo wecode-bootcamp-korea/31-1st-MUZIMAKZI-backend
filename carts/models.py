e0a79c9d5523e6b06a6fda2f3d52bdc221556d0c
gleehave <gleehave@gmail.com>
2022-03-29 17:58:48 +0900

    [이강호][add] cart timestampedmodel 상속받기

--- carts/models.py
+++ carts/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+from users.models import TimeStampedModel
 
-class Cart(models.Model):
+
+class Cart(TimeStampedModel):
     user = models.OneToOneField('users.User', on_delete=models.Model)
     product_option_id = models.ForeignKey('products.products_options', on_delete=models.Model, related_name='carts')
     quantity = models.IntegerField(default=0)
