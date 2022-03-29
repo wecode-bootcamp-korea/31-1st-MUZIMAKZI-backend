3826e9bcad8396cb45b43cc5eee1b251682dc4bd
gleehave <gleehave@gmail.com>
2022-03-29 10:22:33 +0900

    [이강호][add] User 클래스 작성 (migrate 전)

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class User(TimeStampedModel):
+    name         = models.CharField(max_length=45)
+    email        = models.EmailField(max_length=100, unique=True)
+    password     = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return self.name
