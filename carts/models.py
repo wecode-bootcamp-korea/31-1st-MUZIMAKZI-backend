25806d7e12a98a2d7c93edd492e9b7a2a2d505c3
gleehave <gleehave@gmail.com>
2022-04-01 13:59:57 +0900

    [이강호][modify] Cart 와 User 1:M 관계 설정

--- carts/models.py
+++ carts/models.py
@@ -4,7 +4,7 @@ from core.models import TimeStampedModel
 
 
 class Cart(TimeStampedModel):
-    user = models.OneToOneField('users.User', on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='cats')
     product_option = models.ForeignKey('products.ProductOption', on_delete=models.CASCADE, related_name='carts')
     quantity = models.IntegerField(default=0)
 
