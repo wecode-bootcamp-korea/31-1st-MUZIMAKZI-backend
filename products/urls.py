e6861267be517160b851e9e2407aece987c55d11
gleehave <gleehave@gmail.com>
2022-04-01 15:04:23 +0900

    [이강호][add] url 설정

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 
-from products.views import ProductCategoryView
+from products.views import ProductCategoryView, ProductListView
 
 urlpatterns = [
     path('/categories', ProductCategoryView.as_view()),
+    path('/categories/list', ProductListView.as_view())
 ]
