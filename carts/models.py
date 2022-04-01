0cf35c21fd6bba13fcebadb97697f5a588f74fd5
gleehave <gleehave@gmail.com>
2022-04-01 14:18:41 +0900

    [이강호][modify] related_name 수정

--- carts/models.py
+++ carts/models.py
@@ -4,7 +4,7 @@ from core.models import TimeStampedModel
 
 
 class Cart(TimeStampedModel):
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='cats')
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='carts')
     product_option = models.ForeignKey('products.ProductOption', on_delete=models.CASCADE, related_name='carts')
     quantity = models.IntegerField(default=0)
 
