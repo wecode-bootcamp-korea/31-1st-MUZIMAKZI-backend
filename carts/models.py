b04c8d50e17db7c9fbae7de07ef0bace645c7a41
gleehave <gleehave@gmail.com>
2022-04-01 13:39:04 +0900

    [이강호][modify] on_delete 속성 변경

--- carts/models.py
+++ carts/models.py
@@ -4,8 +4,8 @@ from core.models import TimeStampedModel
 
 
 class Cart(TimeStampedModel):
-    user = models.OneToOneField('users.User', on_delete=models.Model)
-    product_option = models.ForeignKey('products.ProductOption', on_delete=models.Model, related_name='carts')
+    user = models.OneToOneField('users.User', on_delete=models.CASCADE)
+    product_option = models.ForeignKey('products.ProductOption', on_delete=models.CASCADE, related_name='carts')
     quantity = models.IntegerField(default=0)
 
     class Meta:
