02c8d91142f5099868fc28b8d1e9707a99a4effc
gleehave <gleehave@gmail.com>
2022-04-02 11:25:29 +0900

    [이강호][add] url: path parameter

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from products.views import ProductCategoryView, ProductListView
 
 urlpatterns = [
-    path('/categories', ProductCategoryView.as_view()),
-    path('/categories/list', ProductListView.as_view())
+    # http://127.0.0.1:8000/products/categories/1
+    path('/categories/<int:category_id>', ProductCategoryView.as_view()),
+
+    # http://127.0.0.1:8000/products/categories/1/list
+    path('/categories/<int:type_id>/list', ProductListView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -6,42 +6,33 @@ from django.views import View
 from products.models import Type, Product
 
 class ProductCategoryView(View):
-    def get(self, request):
-        try:
-            data     = json.loads(request.body)
-            category_id = data['category_id']
-
+    def get(self, request, category_id):
+        #try:
             types = Type.objects.filter(category=category_id)
 
-            result = []
-            for type in types:
-                result.append({
-                    'name'          : type.name,
-                    'thumbnail_url' : type.thumbnail_image_url,
-                })
+            result = [{
+                'name'         : type.name,
+                'thumbnail_url':type.thumbnail_image_url
+            } for type in types]
 
             return JsonResponse({'message': result}, status=200)
 
-        except KeyError:
-            return JsonResponse({'message': 'invalid key'}, status=400)
+        # except KeyError:
+        #     return JsonResponse({'message': 'invalid key'}, status=400)
 
 class ProductListView(View):
-    def post(self, request):
-        try:
-            data = json.loads(request.body)
-            type_id = data['type_id']
-
+    def get(self, request, type_id):
+        #try:
             products = Product.objects.filter(type=type_id)
 
-            result = []
-            for product in products:
-                result.append({
-                    'name': product.name,
-                    'price': product.price,
-                    'thumbnail_image_url': product.thumbnail_image_url
-                })
+            result = [{
+                'name'               : product.name,
+                'price'              : product.price,
+                'thumbnail_image_url': product.thumbnail_image_url
+            } for product in products]
 
             return JsonResponse({'message': result}, status=200)
-        except KeyError:
-            return JsonResponse({'message': 'invalid key'}, status=400)
+
+        # except KeyError:
+        #     return JsonResponse({'message': 'invalid key'}, status=400)
 
