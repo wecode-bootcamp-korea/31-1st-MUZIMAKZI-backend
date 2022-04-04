cbcf03a3d0c5e23babf8d1103853173935b977a1
gleehave <gleehave@gmail.com>
2022-04-04 17:08:50 +0900

    [이강호][fix] migrate 충돌 해결

--- carts/models.py
+++ carts/models.py
@@ -4,9 +4,9 @@ from core.models import TimeStampedModel
 
 
 class Cart(TimeStampedModel):
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    user           = models.ForeignKey('users.User', on_delete=models.CASCADE)
     product_option = models.ForeignKey('products.ProductOption', on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=0)
+    quantity       = models.IntegerField(default=0)
 
     class Meta:
         db_table = 'carts'
