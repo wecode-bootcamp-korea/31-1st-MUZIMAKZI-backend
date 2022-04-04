4b4663c209bed81ca800804ef0df15954cd53d65
gleehave <gleehave@gmail.com>
2022-04-04 16:33:40 +0900

    [이강호][modify] on_delete 옵션 수정

--- carts/models.py
+++ carts/models.py
@@ -4,8 +4,8 @@ from core.models import TimeStampedModel
 
 
 class Cart(TimeStampedModel):
-    user = models.ForeignKey('users.User', on_delete=models.Model)
-    product_option = models.ForeignKey('products.ProductOption', on_delete=models.Model)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    product_option = models.ForeignKey('products.ProductOption', on_delete=models.CASCADE)
     quantity = models.IntegerField(default=0)
 
     class Meta:
--- wishlist/models.py
+++ wishlist/models.py
@@ -4,8 +4,8 @@ from core.models import TimeStampedModel
 
 
 class WishList(TimeStampedModel):
-    user = models.ForeignKey('users.User', on_delete=models.Model)
-    product_option = models.ForeignKey('products.ProductOption', on_delete=models.Model)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    product_option = models.ForeignKey('products.ProductOption', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'wish_lists'
