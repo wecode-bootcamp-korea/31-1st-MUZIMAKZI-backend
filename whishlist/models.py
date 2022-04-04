355b1465d5e40359259fd450dc282048de22011c
gleehave <gleehave@gmail.com>
2022-04-04 17:35:47 +0900

    [이강호][modify] code convention 유지

--- whishlist/models.py
+++ whishlist/models.py
@@ -4,7 +4,7 @@ from core.models import TimeStampedModel
 
 
 class WishList(TimeStampedModel):
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    user           = models.ForeignKey('users.User', on_delete=models.CASCADE)
     product_option = models.ForeignKey('products.ProductOption', on_delete=models.CASCADE)
 
     class Meta:
